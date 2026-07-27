ae9c078ffd8b5bb7493c23effc7a7c5ecf1454d5
Giancarlo-Catalano <g.a.catalano@stir.ac.uk>
2026-07-27 15:23:23 +0100

    Paste code I use very often, to announce when slow code is running, and to show how long it takes. It is utils.announce(msg), a context manager

---
+++ utils.py
@@ -0,0 +1,49 @@
+import time
+from contextlib import ContextDecorator
+
+
+class ExecutionTime(ContextDecorator):
+    start_time: float
+    end_time: float
+    runtime: float
+
+    def __enter__(self):
+        self.start_time = time.time()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.end_time = time.time()
+        self.runtime = self.end_time - self.start_time
+
+    def __str__(self):
+        return f"{self.runtime:.6f}"
+
+
+def execution_timer():
+    return ExecutionTime()
+
+
+class Announce(ContextDecorator):
+    action_str: str
+    timer: ExecutionTime
+    verbose: bool
+
+    def __init__(self, action_str: str, verbose=True):
+        self.action_str = action_str
+        self.timer = ExecutionTime()
+        self.verbose = verbose
+
+    def __enter__(self):
+        if self.verbose:
+            print(f"starting [{self.action_str}]")
+        self.timer.__enter__()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.timer.__exit__(exc_type, exc_val, exc_tb)
+        runtime = self.timer.runtime
+        if self.verbose:
+            print(f"[{self.action_str}]...Finished (took {runtime:2f} seconds)")
+
+
+def announce(action: str, verbose=True):
+    return Announce(action, verbose)
